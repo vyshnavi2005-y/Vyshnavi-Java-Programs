11abe48122c68a8cd1386578c8199505034a3ffe
vyshnavi2005-y <sainavi2005@gmail.com>
2025-08-16 10:09:00 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,20 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] numbers = {5, 3, 8, 4, 2, 7};
+        int target = 4;
+        int result = linearSearch(numbers, target);
+        if (result == -1) {
+            System.out.println("Element not found in the array.");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
